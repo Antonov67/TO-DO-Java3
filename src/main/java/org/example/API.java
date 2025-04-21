b2c3d87aec2c6ee70ca86db6d508ccd31dcc6f44
Dm Ant <Dm@Ant.ru>
2025-04-21 17:38:45 +0300

    Реализовано API

--- src/main/java/org/example/API.java
+++ src/main/java/org/example/API.java
@@ -1,11 +1,20 @@
 package org.example;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
+import retrofit2.http.*;
 
 public interface API {
 
     @GET("collections/todos/records")
     Call<ResponseTodo> getTodos();
+
+    @POST("collections/todos/records")
+    Call<Todo> createTodo(@Body Todo todo);
+
+    @DELETE("collections/todos/records/{id}")
+    Call<Void> deleteTodo(@Path("id") String id);
+
+    @PATCH("collections/todos/records/{id}")
+    Call<Todo> updateTodo(@Path("id") String id, @Body Todo todo);
 
 }
